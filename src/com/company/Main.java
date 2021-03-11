0e5fee8e9f074feadd02b7956c5edf0660771ea6
Xolani Ganta <xolaniganta@gmail.com>
2021-03-12 00:50:52 +0200

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("this is the first git practise");
+        //this is the new branch
     }
 }
